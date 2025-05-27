86f08aa981867d1528b58f1541fd8ab485db9386
taren07 <taro.enchi@gmail.com>
2025-05-28 00:10:53 +0900

    add biome

--- apps/ec/app/cart/checkout/information/page.tsx
+++ apps/ec/app/cart/checkout/information/page.tsx
@@ -1,11 +1,9 @@
-
-
 export default function CheckoutInformationPage() {
-    return (
-        <div>
-        <h1>Checkout Information</h1>
-        <p>Please provide your information to proceed with the checkout.</p>
-        {/* Additional components and logic for checkout information can be added here */}
-        </div>
-    );
-    }
+	return (
+		<div>
+			<h1>Checkout Information</h1>
+			<p>Please provide your information to proceed with the checkout.</p>
+			{/* Additional components and logic for checkout information can be added here */}
+		</div>
+	);
+}
