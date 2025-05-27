10a0a7de39e95c380f6a07df0314e1c9c3fb6bfc
taren07 <taro.enchi@gmail.com>
2025-05-27 23:57:10 +0900

    create cart directory

---
+++ apps/ec/app/cart/checkout/information/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function CheckoutInformationPage() {
+    return (
+        <div>
+        <h1>Checkout Information</h1>
+        <p>Please provide your information to proceed with the checkout.</p>
+        {/* Additional components and logic for checkout information can be added here */}
+        </div>
+    );
+    }
